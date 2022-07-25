631a4f5bcbbfe26e421791e9e181269f6a721d28
Usha <ushadeepika1997@gmail.com>
2022-07-25 14:38:12 -0400

    logic updated

---
+++ src/com/jnit/training/arraysandstrings/Sum.java
@@ -0,0 +1,18 @@
+package com.jnit.training.arraysandstrings;
+
+public class Sum {
+
+	public static void main(String[] args) {
+				
+				
+				int sum = 0;
+				for(int i = 1; i <= 10; i++) {
+				sum = sum + i;
+				System. out. println("Sum of numbers is : " + sum);
+			
+				}
+				
+
+	}
+
+}
